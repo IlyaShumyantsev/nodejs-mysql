80872d5fc3bda0550b583d05f9a652562d2fd701
assetbookilya <ilya.shumyantsev@assetbook.com>
2021-09-23 18:07:02 +0300

    add graphql

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const sequelize = require('./utils/database');
-const todoRoutes = require('./routes/todo');
 const path = require('path');
+const schema = require('./graphql/schema');
+const resolver = require('./graphql/resolver');
 
 const app = express();
 const PORT = process.env.PROT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use('/api/todo', todoRoutes);
+app.use(
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+    graphiql: true,
+  }),
+);
 
 app.use((req, res, next) => {
   res.sendFile('/index.html');
